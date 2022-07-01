d58c496590c8bbfa2fb022aa9e15f9168aa8c4bb
= <henrique.angar@grupoimtraff.com.br>
2022-07-01 15:55:32 -0300

    dataBase has been finished

--- codigo/js/itemper.js
+++ codigo/js/itemper.js
@@ -1,8 +1,21 @@
 
-function findItemPer(disciplina,ocorrencia) {
+let confere = 0; let diferente = 0;
 
-    console.log(disciplina, ocorrencia);
+async function findItemPer(disciplina,ocorrencia) {
 
+    let somar = false;
+
+    let base = await basePer;
+    
+    let findX = false; let findY= false; 
+    let sizeX = base.length; let i = 0;
+
+    while(findX == false || i<sizeX){
+
+        if(base[i].disciplina == disciplina){ findX = true; somar = true;}
+    }
+
+    if(somar == true){ confere++}else{diferente++}
 }
 
 
