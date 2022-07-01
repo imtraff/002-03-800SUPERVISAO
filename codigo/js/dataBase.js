1ea40e0a5f690caa8f4e34a0bab2caffb1a81b80
= <henrique.angar@grupoimtraff.com.br>
2022-07-01 14:44:03 -0300

    dataset importation and configuration finished

---
+++ codigo/js/dataBase.js
@@ -0,0 +1,52 @@
+//Import list from database to compare
+const basePer = importarListaPer();
+
+async function importarListaPer() {
+      
+    let arquivo = await fetch('http://127.0.0.1:5500/codigo/per.csv');
+    const dados = await arquivo.text();
+
+    const conteudo = dados.split('\n').slice(1);
+
+    const listaBruta = criarArray(conteudo);
+    
+    const listaProcessada = processarValores(listaBruta);
+
+    const dataBase = configData(listaProcessada);
+
+    return dataBase;
+}
+
+
+function configData(data){
+
+    let readyData = new Array;
+
+    data.forEach(element => {
+        
+        let object = prepareObject(element);
+        readyData.push(object);
+    });
+
+    return readyData;
+}
+
+function prepareObject(values){
+
+    let object = {
+
+        disciplina: values[0],
+        ocorrencia: splitText(values[1]),
+        parameter: splitText(values[2]),
+        code: values[3],
+        standart: values[4]
+    }
+
+    return object;
+}
+
+function splitText(string){ 
+
+    if(string == undefined || string == null || string == "-" || string =="" || string ==" "){ return "-";}
+    else{ let newText = string.split("?"); return newText;}
+}
--- codigo/js/itemper.js
+++ codigo/js/itemper.js
@@ -1,20 +1,8 @@
+
 function findItemPer(disciplina,ocorrencia) {
 
-    let listaItemPer = importarListaPer();
+    console.log(disciplina, ocorrencia);
 
 }
 
-async function importarListaPer() {
-      
-        let arquivo = await fetch('per.csv');
-        const dados = await arquivo.text();
-    
-        const conteudo = dados.split('\n').slice(1);
-    
-        const listaBruta = criarArray(conteudo);
-        
-        const listaProcessada = processarValores(listaBruta);
-    
-        console.log(listaProcessada);
-        //return dadosProcessados;
-}
+
