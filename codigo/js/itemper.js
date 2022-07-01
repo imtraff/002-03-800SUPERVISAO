e08299a510df50affeb450d4edd00f22d77d81d8
= <henrique.angar@grupoimtraff.com.br>
2022-07-01 17:23:16 -0300

    dataset ajusted

--- codigo/js/itemper.js
+++ codigo/js/itemper.js
@@ -1,7 +1,5 @@
 
-let confere = 0; let diferente = 0;
-
-async function findItemPer(disciplina,ocorrencia) {
+async function findItemPer(discipline,ocorr) {
 
     let somar = false;
 
@@ -10,12 +8,24 @@ async function findItemPer(disciplina,ocorrencia) {
     let findX = false; let findY= false; 
     let sizeX = base.length; let i = 0;
 
-    while(findX == false || i<sizeX){
+    while(findX == false && i<sizeX){
 
-        if(base[i].disciplina == disciplina){ findX = true; somar = true;}
+        if(base[i].disciplina == discipline){ findX = true;
+
+            let list = base[i].ocorrencia; let element = "nothing";
+
+            if(ocorr[0] != false){ element = ocorr[0];
+            }else{
+
+                if(ocorr[1] != false){element = ocorr[1];
+                }else{ element = "Inexistente!";}
+            }
+
+            console.log(element);
+        }
+
+        i++;
     }
-
-    if(somar == true){ confere++}else{diferente++}
 }
 
 
