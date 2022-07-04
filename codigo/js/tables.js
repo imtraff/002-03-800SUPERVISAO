c0caa275b143c7a3a3b47f60aea1f33c10c89102
Henrique Angar <henrique.angar@grupoimtraff.com.br>
2022-07-04 14:12:30 -0300

    table creation done

--- codigo/js/tables.js
+++ codigo/js/tables.js
@@ -3,51 +3,86 @@
 function generateTables(data){
 
     let dataTables = organizeData(data);
+    let tables = createTables(dataTables);
 }
+
+//Section to oraganize data for the tables by disciplina
 
 //Config dataBase to a array of array by disciplina
 function organizeData(data){
 
     let arrayDisciplinas = getDisciplinas(data);
     let agroupDisciplines = mapData(arrayDisciplinas, data);
-
-    //Código para criar as tabelas
+    return agroupDisciplines;
 }
 
-function getDisciplinas(data){
-
-    let i = 0; j = 0;
-    let size = Object.keys(data).length;
-    let arrayDisciplinas = new Array;
+//generates an array of discipline types
+function getDisciplinas(data){ let i = 0; j = 0; let size = Object.keys(data).length; let arrayDisciplinas = new Array;
 
     arrayDisciplinas[0] = data[0].disciplina;
 
-    for(i=0; i<size; i++){
-
-        let add = true;
-        let text = data[i].disciplina;
+    for(i=0; i<size; i++){ let add = true; let text = data[i].disciplina;
 
         arrayDisciplinas.forEach(discipline => { if(text == discipline){ add = false;}});
-
         if(add == true){arrayDisciplinas[j] = text; j++;} 
-    }
-
+    } 
     return arrayDisciplinas;
 }
 
-function mapData(list, data){
+//filter the resgister by discipline type
+function mapData(list, data){ let newDataBase = new Array; let size = Object.keys(data).length;
 
-    let newDataBase = new Array;
-    let size = Object.keys(data).length;
-
-    list.forEach(discipline=>{
-
-        let i = 0; let item = new Array;
+    list.forEach(discipline=>{ let i = 0; let item = new Array;
 
         for(i=0; i<size; i++){if(data[i].disciplina == discipline){ item.push(data[i])};}
 
         newDataBase.push(item);
-    });
+    }); return newDataBase;
+}
 
-    return newDataBase;
+
+//Section to create new Tables
+
+//create an array of all html tables
+function createTables(array){ let place = document.getElementById("tables");
+
+    array.forEach( dataArray =>{ let table = newTable();
+
+        dataArray.forEach(data => { let line = newLine([false, data]); table.appendChild(line);});
+
+        place.appendChild(table);
+    })
+}
+
+//generate a new table with a basic header
+function newTable(){
+
+    let html = document.createElement("div"); html.setAttribute("class", "table_box");
+    let table = document.createElement("table"); table.setAttribute("class", "table_structure");
+
+    let line = newLine([true]);
+
+    table.appendChild(line); html.appendChild(table); return html;
+}
+
+//generate the lines of header and body
+function newLine(content){ const header = content[0]; let data = new Array;
+    
+    let line = document.createElement("tr"); line.setAttribute("class", "table_line");
+
+    if(header){ data = ["ITEM" , "CÓDIGO", "DATA HORA", "SENTIDO", "KM", "LOCAL","ELEMENTO", "OCORRÊNCIAS", "ITEM PER", "PROVIDÊNCIAS"];
+
+    }else{ let object = content[1];
+        data = ["none", object.codigo, object.data+" "+object.hora, object.sentido, object.km, object.local, object.disciplina, object.ocorrencia, object.per, "a definir"];
+    }
+
+    data.forEach(value => { let cell = newCell(value); line.appendChild(cell)});
+
+    return line;
+}
+
+//generate a new cell with value
+function newCell(data){
+
+    let html = document.createElement("td"); html.setAttribute("class", "table_column"); html.innerText = data; return html;
 }
